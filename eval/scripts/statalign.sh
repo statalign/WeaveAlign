198d12431f6e6e8de58ce90b508e908bed9ea76d
1j <herman@stats.ox.ac.uk>
2014-09-08 17:52:51 -0400

    Removed old eval directory.

--- eval/scripts/statalign.sh
+++
@@ -1,1 +0,0 @@
-java -Xmx512m -jar statalign.jar -mcmc=50k,500k,1k -ot=Fasta -log=L,A $1
--- eval/simul/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-perl generate_data.pl
-mafft t091/data.fsa >t091/data.mafft.fsa
-perl ../scripts/score.pl t091/data.fsa t091/data.mafft.fsa
