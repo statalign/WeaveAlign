e77d01391916166353a3504bed444dbb28d0130c
Adrienn Szabo <adrienn.szabo4@gmail.com>
2014-09-09 15:05:59 +0200

    Generated zip archive will contain runnable jars

--- pack-all-to-zip.sh
+++ pack-all-to-zip.sh
@@ -3,6 +3,8 @@
 thisDir=$(dirname $0) || false
 
 pushd ${thisDir} > /dev/null
+  ./build-all.sh
   gradle distZip
+  echo "Created zip archive: build/distributions/WeaveAlign-1.2.zip"
 popd > /dev/null
 
