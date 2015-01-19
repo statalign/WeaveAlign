52d5770e3e0bfc03b4855c629c9a61525b5949d1
1j <herman@stats.ox.ac.uk>
2015-01-19 17:36:27 -0500

    Zip script now does not build -- otherwise adds unnecessary bulk to the archive.

--- pack-all-to-zip.sh
+++ pack-all-to-zip.sh
@@ -3,7 +3,7 @@
 thisDir=$(dirname $0) || false
 
 pushd ${thisDir} > /dev/null
-  ./build-all.sh
+  #./build-all.sh
   gradle distZip
   echo "Created zip archive: build/distributions/WeaveAlign-1.2.zip"
 popd > /dev/null
